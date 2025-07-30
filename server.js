4c611e8caccbdac8caf5a2778161aa572a69700f
mahesh sonsale <maheshsonsale159@gmail.com>
2025-07-30 11:31:21 +0530

    s

--- server.js
+++ server.js
@@ -6,18 +6,24 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser'
 
 
-const PORT=process.env.PORT
+const PORT = process.env.PORT
 const app = express()
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("api running")
 })
 
-app.use(cors({origin:'https://insta-frontend-kohl.vercel.app',credentials:true}))
+app.use(cors({
+    origin: 'https://insta-frontend-kohl.vercel.app',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+}))
 app.use(express.json())
 app.use(cookieParser())
 app.use(rout)
 
+app.options('*', cors())
 
-app.listen(PORT ||5000, () => {
+
+app.listen(PORT || 5000, () => {
     console.log(`Server Running At ${PORT} PORT`);
 })
